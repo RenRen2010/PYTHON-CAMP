21ac96c87d9e6d20c45b42e9cb25afcc0b96a5cd
RenRen2010 <rainierandsol@gmail.com>
2024-07-26 11:19:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-import streamlit as st 
+import streamlit as st
 
-st.title("Testing....")
+st.title("Welcome to My First Streamlit App")
+
+name = st.text_input("Enter your name")
+
+if name:
+  st.write(f"Hello, {name}! Welcome to the app.")
+                     
